980bffe9ba0d244b36f7bf8780ca283112df9f39
EngCamo <learn3038it@gmail.com>
2025-08-28 21:14:54 +0800

    ui update

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function layout({ children }: { children: ReactNode }) {
+  const serverHeaders = await headers();
+  const session = await auth.api.getSession({ headers: serverHeaders });
+  const refress = serverHeaders.get("referer") ?? "/";
+  if (session) redirect(refress);
+
+  return (
+    <main className="h-full w-full flex flex-col justify-center items-center ">
+      {children}
+    </main>
+  );
+}
