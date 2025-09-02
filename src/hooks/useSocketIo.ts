b01fe5e4defb0a76b26c8452e93ff88ae3c33bff
EngCamo <learn3038it@gmail.com>
2025-09-02 16:22:32 +0800

    changes

---
+++ src/hooks/useSocketIo.ts
@@ -0,0 +1,73 @@
+"use client";
+import { io, Socket } from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { ClientToServerEvents, ServerToClientEvents } from "../../types/nexttoexpress";
+
+export function useSocketIo() {
+  const session = authClient.useSession().data;
+  const socketRef = useRef<Socket<ServerToClientEvents, ClientToServerEvents> | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    if (!session?.user?.id) {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+      return;
+    }
+
+    if (!socketRef.current) {
+      socketRef.current = io("http://localhost:4000", {
+        auth: { userId: session.user.id },
+      });
+      console.log("Creating new socket connection.");
+    }
+
+    const socket = socketRef.current;
+    
+
+    const onConnect = () => setIsConnected(true);
+    const onDisconnect = () => setIsConnected(false);
+    
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      console.log("Cleaning up socket connection.");
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+    };
+    
+  }, [session]); 
+
+  function on<EventKey extends keyof ServerToClientEvents>(
+    event: EventKey,
+    callback: ServerToClientEvents[EventKey]
+  ) {
+    socketRef.current?.on(event, callback as any);
+    return () => socketRef.current?.off(event, callback as any);
+  }
+  function off<EventKey extends keyof ServerToClientEvents>(
+    event: EventKey,
+    callback: ServerToClientEvents[EventKey]
+  ) {
+    socketRef.current?.off(event, callback as any);
+    return () => socketRef.current?.off(event, callback as any);
+  }
+
+  function emit<EventKey extends keyof ClientToServerEvents>(
+    event: EventKey,
+    ...args: Parameters<ClientToServerEvents[EventKey]>
+  ) {
+    socketRef.current?.emit(event, ...args);
+  }
+  
+
+  return { socket: socketRef.current, on, emit, isConnected,off };
+}
