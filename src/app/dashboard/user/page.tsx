ca499a9b71e275b2b9f9d7b369697f7f7381b106
xaatim <xayari229@gmail.com>
2025-09-02 12:59:16 +0800

    changes

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,10 +1,12 @@
-import UserDashboardComps from "@/components/UserDashboardComps";
+import RobotComps from "@/components/RobotComps";
+// import UserDashboardComps from "@/components/UserDashboardComps";
 import { Auth, getAllRobotModels, getUserRobots } from "@/lib/serverq";
 import React from "react";
 
 export default async function Page() {
-  const { user } = (await Auth())!;
-  const allUserRobot = await getUserRobots(user.id);
+  const session = await Auth();
+  if (!session) return;
+  const allUserRobot = await getUserRobots(session?.user.id);
 
-  return <UserDashboardComps allRobots={allUserRobot}  />;
+  return <RobotComps robots={allUserRobot} user={session.user!} />;
 }
