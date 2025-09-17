ac35fe310fcc1c9c6894d578922cdeb8714f8b0d
xaatim <xayari229@gmail.com>
2025-09-17 19:00:56 +0800

    changes

--- src/hooks/useRobotStream.ts
+++ src/hooks/useRobotStream.ts
@@ -1,25 +1,30 @@
 import { userRobots } from "@/lib/serverq";
 import { useEffect, useState } from "react";
+import { videoMode } from "../../types/types";
 import { useSocketIo } from "./useSocketIo";
-
-export function useRobotStream(selectedRobot: userRobots | null) {
+type stream = {
+  selectedRobot: userRobots | null;
+  videoMode: videoMode;
+};
+export function useRobotStream({ selectedRobot, videoMode }: stream) {
   const [streamUrl, setStreamUrl] = useState("");
-  const {emit,on,off,socket} = useSocketIo()
+  const { emit, on, off, socket } = useSocketIo();
   useEffect(() => {
     if (!selectedRobot) return;
 
     emit("robot:requestStream", { serialNo: selectedRobot.serialNo });
 
+    console.log("righ before requesting frames");
     const handleStream = (frame: Uint8Array<ArrayBufferLike>) => {
       const blob = new Blob([new Uint8Array(frame)], { type: "image/jpeg" });
-      setStreamUrl(prev => {
+      setStreamUrl((prev) => {
         if (prev) URL.revokeObjectURL(prev);
         return URL.createObjectURL(blob);
       });
     };
 
     const handleStop = () => {
-      setStreamUrl(oldUrl => {
+      setStreamUrl((oldUrl) => {
         if (oldUrl) URL.revokeObjectURL(oldUrl);
         return "";
       });
@@ -31,12 +36,12 @@ export function useRobotStream(selectedRobot: userRobots | null) {
     return () => {
       off("robot:stream", handleStream);
       off("robot:streamStop", handleStop);
-      setStreamUrl(oldUrl => {
+      setStreamUrl((oldUrl) => {
         if (oldUrl) URL.revokeObjectURL(oldUrl);
         return "";
       });
     };
-  }, [selectedRobot, socket]);
+  }, [selectedRobot, socket, videoMode]);
 
   return streamUrl;
 }
